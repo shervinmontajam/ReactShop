b2d5e22da3c75b908e790bb6590a3b723f896d08
shervinmontajam <montajam@gmail.com>
2019-07-25 00:27:57 +0430

    fix map return

--- src/components/menu/RouteList.js
+++ src/components/menu/RouteList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Route, Switch } from "react-router-dom";
+import _ from 'lodash';
 
 import {moduleList} from './ModuleList';
 import Notfound from "../shared/errors/not-found/NotFound";
@@ -8,11 +9,9 @@ import Notfound from "../shared/errors/not-found/NotFound";
 
 const RouteList = ({ pageList }) => {
 
-
-  const routes = pageList.map(page => {
-    if(page.moduleName !== "" && page.type !== "no-action") {
+  const filteredPageList = _.filter(pageList, page => (page.moduleName !== "" && page.type !== "no-action"));
+  const routes = filteredPageList.map(page => {
       return <Route key={page.id} path={page.action} exact component={moduleList.find(item=> item.moduleName === page.moduleName).module} />;
-    }
   });
 
   return (
